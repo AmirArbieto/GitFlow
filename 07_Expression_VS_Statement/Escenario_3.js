d6d80c236078182245a87cb541f618d745e983b4
Amir <amir.arbieto@vallegrande.edu.pe>
2023-09-21 18:02:00 -0500

    Escenario_3

---
+++ 07_Expression_VS_Statement/Escenario_3.js
@@ -0,0 +1,9 @@
+// Paso 1: Definir la lista de compras
+var listaDeCompras = ["leche", "pan", "huevos", "frutas", "detergente"];
+
+// Paso 2: Recorrer la lista de compras con un bucle for
+console.log("Tienes que comprar:");
+for (var i = 0; i < listaDeCompras.length; i++) {
+  // Paso 3: Mostrar cada artículo junto con su posición
+  console.log("   - " + listaDeCompras[i]);
+}
