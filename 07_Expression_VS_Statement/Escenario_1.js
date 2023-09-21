f4a6fef847cdee775753f7812a438caeb7729f28
Amir <amir.arbieto@vallegrande.edu.pe>
2023-09-21 17:56:22 -0500

    Escenario_1

---
+++ 07_Expression_VS_Statement/Escenario_1.js
@@ -0,0 +1,12 @@
+// Paso 1: Definir las variables
+var radio = 3; // Cambia el valor del radio según sea necesario
+var altura = 10; // Cambia el valor de la altura según sea necesario
+
+// Paso 2: Calcular el área de la base
+var areaBase = Math.PI * Math.pow(radio, 2);
+
+// Paso 3: Calcular el volumen del cilindro
+var volumen = areaBase * altura;
+
+// Paso 4: Mostrar el resultado en la consola
+console.log("El volumen del cilindro es: " + volumen);
