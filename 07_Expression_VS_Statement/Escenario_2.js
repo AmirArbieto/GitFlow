2b98d6f717897622a3cf502d095018561ff07ed1
Amir <amir.arbieto@vallegrande.edu.pe>
2023-09-21 18:00:13 -0500

    Escenario_2

---
+++ 07_Expression_VS_Statement/Escenario_2.js
@@ -0,0 +1,11 @@
+// Paso 1: Definir la variable
+var numero = 7; // Cambia el valor del número según sea necesario
+
+// Paso 2: Verificar si es par o impar
+if (numero % 2 === 0) {
+  // Paso 3: Si es par, mostrar un mensaje
+  console.log("El número " + numero + " es par.");
+} else {
+  // Paso 4: Si es impar, mostrar otro mensaje
+  console.log("El número " + numero + " es impar.");
+}
